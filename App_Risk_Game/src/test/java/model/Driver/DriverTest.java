cff5cde848822748fdffafb93f92f6fade07152f
Mutesham <sardarms9@gmail.com>
2020-02-21 15:43:28 -0500

    Driver testcase added

---
+++ App_Risk_Game/src/test/java/model/Driver/DriverTest.java
@@ -0,0 +1,15 @@
+package src.test.java.model.Driver;
+
+import org.junit.Test;
+
+import src.main.java.Controller.Driver;
+
+public class DriverTest {
+
+	@Test
+	public void testDriver() {
+		Driver driverobj = new Driver();
+		driverobj.setNum(7);
+	}
+
+}
